88e9b6dc584b5f153e056cfb6414beb593a06757
Aarif Khan <ajajchauhan@gmail.com>
2023-10-22 14:52:26 +0530

    Add Basic Token Auth

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
+
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
+// Check for token
+app.use((req, res, next)=>{
+    const token = req.body.token;
+    if(token != "wewillwin"){
+        console.log("Unauthorized Access.");
+        res.sendStatus(401);
+        res.end();
+    }else{
+        next();
+    }
+});
 
 app.get('/', (req, res)=>{
     res.send("Hello, World!");
