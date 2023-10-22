8053bc6905c887590def8171871237daa89e4299
Aarif Khan <ajajchauhan@gmail.com>
2023-10-22 18:22:37 +0530

    Connect to Firebase, Get GeoJSON

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
 const app = express();
 const port = 3000;
 
-app.use(morgan('combined'));
+// Firebase Imports
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set, onValue, get } from "firebase/database";
+
+// Firebase Config
+const firebaseConfig = {
+    apiKey: "AIzaSyC5FbQl7TWhuQYGw4AWzHa-uYdVKHV2ts4",
+    authDomain: "vigyan-69094.firebaseapp.com",
+    projectId: "vigyan-69094",
+    storageBucket: "vigyan-69094.appspot.com",
+    messagingSenderId: "32379019984",
+    appId: "1:32379019984:web:0af45c5f6348cc487a34d5",
+    measurementId: "G-TE9SNXT3S4",
+    databaseURL: "https://vigyan-69094-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+// Initialise Firebase
+const fbapp = initializeApp(firebaseConfig);
+const database = getDatabase(fbapp);
+
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
@@ -20,10 +40,28 @@ app.use((req, res, next)=>{
     }
 });
 
+// Endpoints
+
 app.get('/', (req, res)=>{
-    res.send("Hello, World!");
+    get(ref(database, '/')).then((snapshot)=>{
+        if(snapshot.exists()){
+            console.log("Data fetched for /");
+            res.json(snapshot.val());
+        }else{
+            console.log("No data available");
+        }
+    }).catch((error) => 
+        console.log(error)
+    );
 });
 
+// Start the Server
 app.listen(port, ()=>{
+    // write data
+    // set(ref(database, 'users/'), {
+    //     username: "aarifkhan",
+    //     password: "abcd1234"
+    // });
+
     console.log("OK");
 })
